fc69b270338900fdca5a2af25db1ab578fcdc00e
JohnCoene <jcoenep@gmail.com>
2019-08-01 15:02:13 +0200

    corpora examples added

--- R/corpora.R
+++ R/corpora.R
@@ -5,6 +5,10 @@
 #' @param docs A list of documents as returned by \code{\link{prepare_documents}}.
 #' @param file Path to save \code{.dict}, If \code{NULL} the dictionary is \emph{not} saved.
 #' 
+#' @examples
+#' docs <- prepare_documents(corpus)
+#' (dict <- corpora_dictionary(docs))
+#' 
 #' @export
 corpora_dictionary <- function(docs, file = NULL){
   assert_that(!missing(docs), msg = "Missing `docs`")
@@ -24,6 +28,11 @@ corpora_dictionary <- function(docs, file = NULL){
 #' @param dictionary A dictionary as returned by \code{\link{corpora_dictionary}}.
 #' @param docs A list of documents as returned by \code{\link{prepare_documents}}.
 #' 
+#' @examples
+#' docs <- prepare_documents(corpus)
+#' dict <- corpora_dictionary(docs)
+#' (corpora <- doc2bow(dict, docs))
+#' 
 #' @details Counts the number of occurrences of each distinct word, 
 #' converts the word to its integer word id and returns the result as a sparse vector. 
 #' 
@@ -42,18 +51,29 @@ doc2bow <- function(dictionary, docs){
 
 #' Serialise Matrix Market Corpus
 #' 
-#' Serialise a term-document matrix on disk.
+#' Serialise a term-document matrix to disk.
 #'
 #' @param corpus A corpus as returned by \code{\link{doc2bow}}.
 #' @param file Path to a \code{.mm} file (recommended), if \code{NULL} it is saved to a temp file.
 #' @param auto_delete Wether to automatically delete the temp file after first use.
 #' 
+#' @examples
+#' docs <- prepare_documents(corpus)
+#' dict <- corpora_dictionary(docs)
+#' corpora <- doc2bow(dict, docs)
+#' 
+#' # serialize and delete
+#' \dontrun{corpus_mm <- serialize_mmcorpus(corpora)}
+#' \dontrun{delete_mmcorpus(corpus_mm)}
+#' 
 #' @section Functions:
 #' \itemize{
 #'   \item{\code{serialize_mmcorpus} - Serialize the corpus}  
-#'   \item{\code{as_serialized_mmcorpus} - Create an \code{mm_file} from an already created corpus file.}  
+#'   \item{\code{as_serialized_mmcorpus} - Create an object of class \code{mm_file} from an already created corpus file.}  
 #'   \item{\code{delete_mmcorpus} - Delete temp corpus.}  
 #' }
+#'
+#' @details Serialize the corpus to disk in order to take advantage of Python's file scan efficiency.
 #' 
 #' @return An object of class \code{mm_file} which holds the path to the \code{file} and metadata.
 #' 
@@ -119,11 +139,27 @@ print.mm_file <- function(x, ...){
 
 #' Read Serialized Matrix Market
 #' 
-#' Read Serialized Matrix Market from path or \code{\link{as_serialized_mmcorpus}}.
+#' Read Serialized Matrix Market from path or \code{\link{as_serialized_mmcorpus}}. 
 #' 
 #' @param file path or return value of \code{\link{as_serialized_mmcorpus}}.
 #' 
+#' @examples
+#' docs <- prepare_documents(corpus)
+#' dict <- corpora_dictionary(docs)
+#' corpora <- doc2bow(dict, docs)
+#' 
+#' # file to hold matrix market
+#' fl <- "corpus.mm"
+#' 
+#' # serialize
+#' \dontrun{corpus_mm <- serialize_mmcorpus(corpora, file = fl)}
+#' 
+#' # read and delete
+#' \dontrun{(mm <- read_serialized_mmcorpus(corpus_mm))}
+#' \dontrun{delete_mmcorpus(corpus_mm)}
+#' 
 #' @name read_serialized_mmcorpus
+#' 
 #' @export
 read_serialized_mmcorpus <- function(file) UseMethod("read_serialized_mmcorpus")
 
@@ -133,6 +169,8 @@ read_serialized_mmcorpus <- function(file) UseMethod("read_serialized_mmcorpus")
 read_serialized_mmcorpus.mm_file <- function(file){
   assert_that(!missing(file), msg = "Missing `file`")
   mm <- .as_mm(gensim$corpora$MmCorpus(file$file))
+  # unlink temp
+  if(file$temp && file$delete) unlink(file$file)
   invisible(mm)
 }
 
@@ -142,6 +180,8 @@ read_serialized_mmcorpus.mm_file <- function(file){
 read_serialized_mmcorpus.character <- function(file){
   assert_that(!missing(file), msg = "Missing `file`")
   mm <- .as_mm(gensim$corpora$MmCorpus(file))
+  # unlink temp
+  if(file$temp && file$delete) unlink(file$file)
   invisible(mm)
 }
 
@@ -151,6 +191,12 @@ read_serialized_mmcorpus.character <- function(file){
 #'
 #' @param file Path to \code{.txt} file.
 #' @param max_sentence_length Maximum length of sentence to scan.
+#' 
+#' @examples
+#' file <- datapath('testcorpus.txt') # example file
+#' readLines(file) # what it looks like
+#' 
+#' (sentences <- text8corpus(file))
 #' 
 #' @export
 text8corpus <- function(file, max_sentence_length = 10000L){
