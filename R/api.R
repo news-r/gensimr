83dee80c3c6b88a650c6fa8f89a073723463d725
JohnCoene <jcoenep@gmail.com>
2019-08-01 11:34:09 +0200

    examples added

--- R/api.R
+++ R/api.R
@@ -4,14 +4,27 @@
 #' See [RaRe-Technologies/gensim-data](https://github.com/RaRe-Technologies/gensim-data) 
 #' repo for more information about models/datasets/how-to-add-new/etc.
 #' 
-#' @param name A specific dataset, i.e.: \code{text8}
+#' @param name A specific dataset, i.e.: \code{text8}.
+#' @param to_r Whether to return the information in R data structure (\code{list}).
 #' @param ... Any other argument from the \href{https://radimrehurek.com/gensim/downloader.html}{official documentation}.
+#' 
+#' @examples
+#' \dontrun{
+#' # available models to use with `api_load`
+#' (models <- api_info() %>% names())
+#' 
+#' # load random model
+#' api_load(sample(models, 1))
+#' }
 #' 
 #' @name api
 #' 
 #' @export
-api_info <- function(name = NULL, ...){
-  gensim$downloader$info(name = name, ...)
+api_info <- function(name = NULL, ..., to_r = TRUE){
+  info <- gensim$downloader$info(name = name, ...)
+  if(to_r)
+    info <- reticulate::py_to_r(info)
+  return(info)
 }
 
 #' @rdname api
--- R/coherence.R
+++ R/coherence.R
@@ -4,7 +4,47 @@
 #' 
 #' @param models A model, i.e.: LDA or LSI, or a \code{list} of the latter.
 #' @param ... Any other options, from the \href{https://radimrehurek.com/gensim/models/coherencemodel.html}{official documentation}.
-#'  
+#' 
+#' @details A greater coherence is preferred: a higher value on the \code{get_coherence} method, see example.
+#' 
+#' @examples
+#' # preprocess the corpus
+#' texts <- prepare_documents(corpus)
+#' dictionary <- corpora_dictionary(texts)
+#' corpus <- doc2bow(dictionary, texts)
+#' 
+#' # create 2 models to compare
+#' good_lda_model <- model_lda(
+#'   corpus = corpus, 
+#'   id2word = dictionary, 
+#'   iterations = 50L, 
+#'   num_topics = 2L
+#' )
+#' bad_lda_model <- model_lda(
+#'   corpus = corpus, 
+#'   id2word = dictionary, 
+#'   iterations = 1L, 
+#'   num_topics = 5L
+#' )
+#' 
+#' # create coherence models
+#' good_cm <- model_coherence(
+#'   model = good_lda_model, 
+#'   corpus = corpus, 
+#'   dictionary = dictionary, 
+#'   coherence = 'u_mass'
+#' )
+#' bad_cm <- model_coherence(
+#'   model = bad_lda_model, 
+#'   corpus = corpus, 
+#'   dictionary = dictionary, 
+#'   coherence = 'u_mass'
+#' )
+#' 
+#' # compare coherence
+#' good_cm$get_coherence()
+#' bad_cm$get_coherence()
+#' 
 #' @name topics
 #' @export
 model_coherence <- function(models, ...) UseMethod("model_coherence")
