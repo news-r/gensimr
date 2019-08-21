e0ddcc5b26a7af791ac7000ea9492bacf5b7d6a2
John Coene <jcoenep@gmail.com>
2019-08-21 17:39:32 +0200

    improved similarity functions

--- R/generic.R
+++ R/generic.R
@@ -3,9 +3,12 @@
 #' Wraps \code{y} with \code{x}
 #' 
 #' @param x,y Python objects.
+#' @param to_r Applies \link[reticulate]{py_to_r} on output.
 #' 
 #' @export
-wrap <- function(x, y) {
+wrap <- function(x, y, to_r = FALSE) {
   wrap <- x[y]
+  if(to_r)
+    return(reticulate::py_to_r(wrap))
   structure(wrap, class = c(class(wrap), "wrapped"))
 }
--- R/similarity.R
+++ R/similarity.R
@@ -21,7 +21,7 @@ similarity_matrix.wrapped <- function(corpus, ...){
 #' 
 #' @param sim A wrapped similarity, output of \code{\link{wrap}}.
 #' 
-#' @return Documents by order of similarity, 
+#' @return A \link[tibble]{tibble} Documents by order of similarity, 
 #' the most similar at the top and the least similar at the bottom.
 #' 
 #' @name get_similarity
@@ -42,4 +42,51 @@ get_similarity.wrapped <- function(sim){
   ) %>% 
     dplyr::mutate(doc = doc - 1) %>% 
     dplyr::arrange(-cosine)
+}
+
+#' Similarity
+#' 
+#' Splits the index into several smaller sub-indexes (“shards”), 
+#' which are disk-based. If your entire index fits in memory 
+#' (~one million documents per 1GB of RAM), you can also use 
+#' the \code{\link{similarity_matrix}}. It is more simple but
+#' does not scale as well: it keeps the entire index in RAM, 
+#' no sharding. It also do not support adding new document 
+#' to the index dynamically.
+#' 
+#' @param corpus A corpus.
+#' @param num_features Size of the dictionary i.e.:\code{reticulate::py_len(dictionary)}.
+#' @param ... Any other parameters to pass to the Python function, see 
+#' \href{https://radimrehurek.com/gensim/similarities/docsim.html#gensim.similarities.docsim.Similarity}{official documentation}.
+#' 
+#' @name similarity
+#' @export
+similarity <- function(corpus, ...) UseMethod("similarity")
+
+#' @rdname similarity
+#' @method similarity gensim.corpora.mmcorpus.MmCorpus
+#' @export
+similarity.gensim.corpora.mmcorpus.MmCorpus <- function(corpus, num_features, ...){
+  file <- tempdir()
+  num_features <- as.integer(num_features)
+  gensim$similarities$Similarity(file, corpus, num_features, ...)
+}
+
+#' @rdname similarity
+#' @method similarity mm_file
+#' @export
+similarity.mm_file <- function(corpus, num_features, ...){
+  file <- tempdir()
+  num_features <- as.integer(num_features)
+  corpus <- read_serialized_mmcorpus(corpus)
+  gensim$similarities$Similarity(file, corpus, num_features, ...)
+}
+
+#' @rdname similarity
+#' @method similarity python.builtin.tuple
+#' @export
+similarity.python.builtin.tuple <- function(corpus, num_features, ...){
+  file <- tempdir()
+  num_features <- as.integer(num_features)
+  gensim$similarities$Similarity(file, corpus, num_features, ...)
 }
