f99609fba5040a6490f16e44b0908ae6a66259b4
John Coene <jcoenep@gmail.com>
2019-08-22 11:18:47 +0200

    callbacks fix attempts #1

--- R/callbacks.R
+++ R/callbacks.R
@@ -7,34 +7,52 @@
 #' @name callbacks
 #' @export
 coherence_metric <- function(...) {
-  cb <- gensim$models$callbacks$CoherenceMetric(...)
+  cb <- gensim$models$callbacks$CoherenceMetric(...) %>% 
+    .construct_callback()
   invisible(cb)
 }
 
 #' @rdname callbacks
 #' @export
 perplexity_metric <- function(...) {
-  cb <- gensim$models$callbacks$PerplexityMetric(...)
+  cb <- gensim$models$callbacks$PerplexityMetric(...) %>% 
+    .construct_callback()
   invisible(cb)
 }
 
 #' @name callbacks
 #' @export
 diff_metric <- function(...) {
-  cb <- gensim$models$callbacks$DiffMetric(...)
+  cb <- gensim$models$callbacks$DiffMetric(...) %>% 
+    .construct_callback()
   invisible(cb)
 }
 
 #' @name callbacks
 #' @export
 convergence_metric <- function(...) {
-  cb <- gensim$models$callbacks$ConvergenceMetric(...)
+  cb <- gensim$models$callbacks$ConvergenceMetric(...) %>% 
+    .construct_callback()
   invisible(cb)
 }
 
 #' @rdname callbacks
 #' @export
 combine_metrics <- function(...) {
-  metrics <- reticulate::tuple(...)
+  metrics <- reticulate::tuple(...) %>% 
+    .construct_callback()
   invisible(metrics)
+}
+
+.construct_callback <- function(x){
+  structure(x, class = c("callback", class(x)))
+}
+
+#' @export
+print.callback <- function(x, ...){
+  cat(
+    crayon::blue(cli::symbol$info),
+    "A callback function\n",
+    ...
+  )
 }
