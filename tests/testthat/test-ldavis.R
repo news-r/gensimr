76e362dabb5726234e9854973df325588d86a515
John Coene <jcoenep@gmail.com>
2019-08-23 14:25:54 +0200

    test ldavis added

---
+++ tests/testthat/test-ldavis.R
@@ -0,0 +1,23 @@
+test_that("multiplication works", {
+  docs <- prepare_documents(corpus)
+  dict <- corpora_dictionary(docs)
+  corpora <- doc2bow(dict, docs)
+  
+  # lda model
+  model <- model_lda(
+    corpus = corpora, 
+    id2word = dict, 
+    iterations = 50L, 
+    num_topics = 2L
+  )
+  
+  # visualise
+  vis <- prepare_ldavis(model, corpora, dict)
+  expect_type(vis, "environment")
+  plot(vis)
+
+  html <- ldavis_as_html(vis)
+  expect_type(html, "environment")
+
+  plot_ldavis(model, corpora, dict)
+})
