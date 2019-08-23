621196fe554ee51d6948fd83830b9068f2fd1827
John Coene <jcoenep@gmail.com>
2019-08-23 16:48:03 +0200

    fixed all tests

--- tests/testthat/test-similarity.R
+++ tests/testthat/test-similarity.R
@@ -1,12 +1,19 @@
 test_that("similarity works", {
-  lsi <- model_lsi(common_corpus(), id2word = common_dictionary())
-
+  data(corpus, package = "gensimr")
+  docs <- prepare_documents(corpus)
+  dictionary <- corpora_dictionary(docs)
+  corpus_bow <- doc2bow(dictionary, docs)
+  corpus_mm <- serialize_mmcorpus(corpus_bow, auto_delete = FALSE)
   mm <- read_serialized_mmcorpus(corpus_mm)
+
+  lsi <- model_lsi(corpus_bow, id2word = dictionary)
 
   new_document <- "A human and computer interaction"
   preprocessed_new_document <- preprocess(new_document, min_freq = 0)
-  vec_bow <- doc2bow(dictionary, preprocessed_new_document)
+  vec_bow <- doc2bow(common_dictionary(), preprocessed_new_document)
   vec_lsi <- wrap(lsi, vec_bow)
+
+  mm <- read_serialized_mmcorpus(corpus_mm)
 
   wrapped_lsi <- wrap(lsi, mm)
   index <- similarity_matrix(wrapped_lsi)
