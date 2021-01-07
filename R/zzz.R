8e7f4408501e954c50499c2dd4dcf6bbf415af51
John (JP) Coene <jcoenep@gmail.com>
2021-01-07 14:54:34 +0100

    change onLoad in attempt to fix Mac install error

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 gensim <- sklearn <- ldavis <- bi <- NULL
 
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(...) {
   gensim <<- reticulate::import("gensim", delay_load = TRUE, convert = FALSE)
   sklearn <<- reticulate::import("sklearn", delay_load = TRUE, convert = FALSE)
   ldavis <<- reticulate::import("pyLDAvis", delay_load = TRUE, convert = FALSE)
